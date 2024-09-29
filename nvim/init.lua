e503f0ebcc790a086b5df4e28723c249ff466e4b
nex <maxinies00@gmail.com>
2024-09-29 18:46:15 +0200

    update(nvim): clean up init function for auto updating

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,10 +2,15 @@ require("pck")
 require("set")
 require("remap")
 
+function sync_all()
+  require("packer").sync()
+
+  vim.cmd("TSUpdate")
+  vim.cmd("MasonUpdate")
+end
+
 vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
 
-vim.cmd([[autocmd VimEnter * lua require('packer').sync()]])
-vim.cmd([[autocmd VimEnter * TSUpdate]])
-vim.cmd([[autocmd VimEnter * MasonUpdate]])
+vim.cmd([[autocmd VimEnter * lua sync_all()]])
 
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
