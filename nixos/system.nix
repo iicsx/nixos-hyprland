56b5694c16535250fac0c73265ae0a18ddfb0abf
pneumaea <maxinies00@gmail.com>
2025-02-15 00:52:48 +0100

    update(linux): reset kernel version to latest
    this was temporarily set to a fixed version to avoid a build error in the latest version
    this has since been fixed

--- nixos/system.nix
+++ nixos/system.nix
@@ -78,7 +78,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_6_12;
+    kernelPackages = pkgs.linuxPackages_latest;
   };
 
   system.stateVersion = "23.05";
